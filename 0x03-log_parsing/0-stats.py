f0f8ff034df01548c50fcc7a205e4c72235ffed3
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-22 00:20:55 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,47 @@
 #!/usr/bin/python3
-from collections import defaultdict
-import signal
 import sys
 
-def print_statistics(stats):
-    total_size = sum(stats["file_sizes"])
-    print(f"Total file size: {total_size}")
-
-    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-    for code in status_codes:
-        if code in stats["status_codes"]:
-            print(f"{code}: {stats['status_codes'][code]}")
-
-def signal_handler(signal, frame):
-    print_statistics(stats)
-    sys.exit(0)
-
 def parse_line(line):
+    # Sample input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    parts = line.split()
+    if len(parts) != 10 or not parts[8].isdigit():
+        return None
+    status_code = int(parts[8])
+    file_size = int(parts[9])
+    return status_code, file_size
+
+def print_statistics(total_size, status_code_counts):
+    print("Total file size:", total_size)
+    for status_code in sorted(status_code_counts.keys()):
+        print(f"{status_code}: {status_code_counts[status_code]}")
+
+def main():
+    total_size = 0
+    status_code_counts = {}
+
     try:
-        parts = line.strip().split()
-        ip_address = parts[0]
-        status_code = int(parts[-3])
-        file_size = int(parts[-1])
-        return status_code, file_size
-    except (IndexError, ValueError):
-        return None, None
-
-stats = {
-    "status_codes": defaultdict(int),
-    "file_sizes": []
-}
-
-line_count = 0
-
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
-    for line in sys.stdin:
-        status_code, file_size = parse_line(line)
-        if status_code is not None and file_size is not None:
-            stats["status_codes"][status_code] += 1
-            stats["file_sizes"].append(file_size)
+        line_count = 0
+        for line in sys.stdin:
             line_count += 1
+            parsed_line = parse_line(line)
+            if parsed_line is None:
+                continue
 
-        if line_count % 10 == 0:
-            print_statistics(stats)
+            status_code, file_size = parsed_line
+            total_size += file_size
 
-except KeyboardInterrupt:
-    pass
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+            else:
+                status_code_counts[status_code] = 1
 
-print_statistics(stats)
+            if line_count % 10 == 0:
+                print_statistics(total_size, status_code_counts)
+
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_code_counts)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
