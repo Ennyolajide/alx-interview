c90866c635d17f22232631e9a951c63cdf9cac62
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-22 00:52:05 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,34 +12,39 @@ def print_statistics(status_code_counts, total_file_size):
             print("{}: {:d}".format(status_code, count))
 
 
-status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-line_count = 0
-total_file_size = 0
+def main():
+    status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    line_count = 0
+    total_file_size = 0
 
-try:
-    for line in sys.stdin:
-        line_count += 1
+    try:
+        for line in sys.stdin:
+            line_count += 1
 
-        if line_count % 10 == 0:
-            print_statistics(status_code_counts, total_file_size)
+            if line_count % 10 == 0:
+                print_statistics(status_code_counts, total_file_size)
 
-        split_line = line.split()
+            split_line = line.split()
 
-        try:
-            file_size = int(split_line[-1])
-            total_file_size += file_size
-        except ValueError:
-            pass
+            try:
+                file_size = int(split_line[-1])
+                total_file_size += file_size
+            except (ValueError, IndexError):
+                pass
 
-        try:
-            status_code = split_line[-2]
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-        except IndexError:
-            pass
+            try:
+                status_code = split_line[-2]
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+            except IndexError:
+                pass
 
-    print_statistics(status_code_counts, total_file_size)
+        print_statistics(status_code_counts, total_file_size)
 
-except KeyboardInterrupt:
-    print_statistics(status_code_counts, total_file_size)
-    raise
+    except KeyboardInterrupt:
+        print_statistics(status_code_counts, total_file_size)
+        raise
+
+
+if __name__ == "__main__":
+    main()
