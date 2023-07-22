5f4e8eba2ff58c5964760a02d4ff2a668b794a4f
Olajide Eniseyin <eniseyinolajide@gmail.com>
2023-07-22 01:20:27 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 
 import sys
 
-
 def print_statistics(status_code_counts, total_file_size):
     """Prints the statistics"""
     print("File size:", total_file_size)
@@ -10,41 +9,53 @@ def print_statistics(status_code_counts, total_file_size):
         if count != 0:
             print(f"{status_code}: {count}")
 
+def main():
+    status_code_counts = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    total_file_size = 0
+    line_count = 0
 
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
-total_file_size = 0
-line_count = 0
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            parsed_line = line.strip().split()
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parsed_line = line.strip().split()
+            if len(parsed_line) == 7:
+                try:
+                    status_code = int(parsed_line[-2])
+                    file_size = int(parsed_line[-1])
+                    total_file_size += file_size
 
-        if len(parsed_line) == 7:
-            try:
-                status_code = int(parsed_line[-2])
-                file_size = int(parsed_line[-1])
-                total_file_size += file_size
+                    if status_code in status_code_counts:
+                        status_code_counts[status_code] += 1
+                except ValueError:
+                    pass
 
-                if status_code in status_code_counts:
-                    status_code_counts[status_code] += 1
-            except ValueError:
-                pass
+            if line_count % 10 == 0:
+                print_statistics(status_code_counts, total_file_size)
+                status_code_counts = {  # Reset the dictionary
+                    200: 0,
+                    301: 0,
+                    400: 0,
+                    401: 0,
+                    403: 0,
+                    404: 0,
+                    405: 0,
+                    500: 0
+                }
+                total_file_size = 0
 
-        if line_count % 10 == 0:
-            print_statistics(status_code_counts, total_file_size)
-            line_count = 0
-            total_file_size = 0
+    except KeyboardInterrupt:
+        print_statistics(status_code_counts, total_file_size)
+        sys.exit(0)
 
-except KeyboardInterrupt:
-    print_statistics(status_code_counts, total_file_size)
-    sys.exit(0)
+if __name__ == "__main__":
+    main()
